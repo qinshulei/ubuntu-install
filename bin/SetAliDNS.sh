6fbc306df042b029dff9b15fbf0b7d2aa4ce7550
qinshulei <527072230@qq.com>
2015-02-13 09:51:39 +0800

    add some bin script

---
+++ bin/SetAliDNS.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+head=/etc/resolvconf/resolv.conf.d/head
+config_resolvconf(){
+	cp -a ${head} ${head}.save
+	cat >${head} <<EOF
+options attempts=2 timeout=2 rotate
+nameserver 223.5.5.5
+nameserver 223.6.6.6
+EOF
+	/sbin/resolvconf -u
+	
+	echo "Reconfig /etc/resolv.conf success."
+}
+
+restore_resolvconf(){
+	if [ -e ${head}.save ];then
+		cat ${head}.save > ${head}
+	else
+		true > ${head}
+	fi
+
+	/sbin/resolvconf -u
+	
+	echo "Resotre /etc/resolv.conf success."
+}
+
+action=$1
+
+if [ $(cat /etc/issue |grep -iwc ubuntu) -ne 1 ];then
+	echo "Only support Ubuntu OS"
+	exit
+fi
+
+if [ "${action}" != "restore" ];then
+	config_resolvconf
+else
+	restore_resolvconf
+fi
+
+echo '--------------------------------------'
+cat /etc/resolv.conf
+
+#End
---
+++ bin/fix-internet-after-vpn.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo route add -net 0.0.0.0 netmask 0.0.0.0 gw 192.168.65.1
